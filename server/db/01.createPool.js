2192aed3d5ec1aafccba9acfc9f85d7581788e55
Bhargav Kulkarni <coderbhargav8237@gmail.com>
2026-04-12 11:00:57 +0530

    fix: updated .ignore

--- server/db/01.createPool.js
+++ server/db/01.createPool.js
@@ -1,6 +1,8 @@
 import {Pool} from 'pg';
 import 'dotenv/config';
 
+//This file opens pool of TCP connections from db to postgres server.
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
