ce753986ab83abfd94d71a67551ad27de8c94cab
Bhargav Kulkarni <coderbhargav8237@gmail.com>
2026-06-01 20:17:27 +0530

    chore: removed db files

--- server/db/07.processBatch.js
+++
@@ -1,9 +0,0 @@
-import { createBatch } from "./06.createBatch.js";
-
-async function run() {
-  console.log("Starting data pipeline...");
-  await createBatch();
-  console.log("Data pipeline completed");
-}
-
-run();
